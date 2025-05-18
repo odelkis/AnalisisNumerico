9113e3116879cdd0a36ded4fdfdd44217b97ef71
odelkis <odelkiseaj539@gmail.com>
2025-05-17 19:47:01 -0500

    Eliminar carpeta coffee_shop

--- coffee_shop/orders/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import OrderProduct
-
-class OrderProductForm(ModelForm):
-    class Meta:
-        model = OrderProduct
-        fields = ['product']
--- coffee_shop/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductFormView, ProductListView
-
-urlpatterns = [
-    path('', ProductListView.as_view(), name='list_product'),
-    path('agregar/', ProductFormView.as_view(), name="add_product"),
-]
