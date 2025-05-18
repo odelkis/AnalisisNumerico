1e0ab663ea692e6e6e2c8b33fe21ed8354e8d354
odelkis <odelkiseaj539@gmail.com>
2025-05-17 19:41:05 -0500

    Subir archivos del proyecto

---
+++ coffee_shop/orders/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Order, OrderProduct
+
+class OrderProductInLineAdmin(admin.TabularInline):
+    model = OrderProduct
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [
+        OrderProductInLineAdmin
+    ]
+
+admin.site.register(Order, OrderAdmin)
